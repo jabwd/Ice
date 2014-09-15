3ce2b2880d1dbeae2e155aeea16deff0a335f570
Antwan van Houdt <jabwd@exurion.com>
2014-09-15 10:05:23 +0200

    Initial Commit

---
+++ Ice/EXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EXAppDelegate.h
+//  Ice
+//
+//  Created by Antwan van Houdt on 15/09/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Ice/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Ice
+//
+//  Created by Antwan van Houdt on 15/09/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
