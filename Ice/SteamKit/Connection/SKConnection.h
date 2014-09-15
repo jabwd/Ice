326fcdaf5a73e49f46ad7f21aa9a77bc635fce3c
Antwan van Houdt <jabwd@exurion.com>
2014-09-15 10:31:14 +0200

    Added the SKConnection file

---
+++ Ice/SteamKit/Connection/SKConnection.h
@@ -0,0 +1,13 @@
+//
+//  SKConnection.h
+//  Ice
+//
+//  Created by Antwan van Houdt on 15/09/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKConnection : NSObject
+
+@end
---
+++ Ice/SteamKit/Connection/SKConnection.m
@@ -0,0 +1,13 @@
+//
+//  SKConnection.m
+//  Ice
+//
+//  Created by Antwan van Houdt on 15/09/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import "SKConnection.h"
+
+@implementation SKConnection
+
+@end
