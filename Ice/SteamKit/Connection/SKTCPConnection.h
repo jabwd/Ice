6cfeb11b7c60835cd578c90a30aa10681e5d3b98
Antwan van Houdt <jabwd@exurion.com>
2014-09-22 17:41:38 +0200

    Added SKTCPConnection and SKSession

---
+++ Ice/SteamKit/Connection/SKTCPConnection.h
@@ -0,0 +1,13 @@
+//
+//  SKTCPConnection.h
+//  Ice
+//
+//  Created by Antwan van Houdt on 18/09/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import "SKConnection.h"
+
+@interface SKTCPConnection : SKConnection
+
+@end
---
+++ Ice/SteamKit/Connection/SKTCPConnection.m
@@ -0,0 +1,13 @@
+//
+//  SKTCPConnection.m
+//  Ice
+//
+//  Created by Antwan van Houdt on 18/09/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import "SKTCPConnection.h"
+
+@implementation SKTCPConnection
+
+@end
