12c6844fe9203731a686ee582265716241c78135
Antwan van Houdt <jabwd@exurion.com>
2014-10-27 04:05:58 +0100

    Added a Protobuf compiler class

---
+++ Ice/SteamKit/Packet/Protobuf/SKProtobufCompiler.h
@@ -0,0 +1,13 @@
+//
+//  SKProtobufCompiler.h
+//  Ice
+//
+//  Created by Antwan van Houdt on 27/10/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKProtobufCompiler : NSObject
+
+@end
---
+++ Ice/SteamKit/Packet/Protobuf/SKProtobufCompiler.m
@@ -0,0 +1,13 @@
+//
+//  SKProtobufCompiler.m
+//  Ice
+//
+//  Created by Antwan van Houdt on 27/10/14.
+//  Copyright (c) 2014 Exurion. All rights reserved.
+//
+
+#import "SKProtobufCompiler.h"
+
+@implementation SKProtobufCompiler
+
+@end
