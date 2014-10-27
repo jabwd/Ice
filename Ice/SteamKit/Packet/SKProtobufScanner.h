34883d6a443f2d84e11ea245262f4247de9b3152
Antwan van Houdt <jabwd@exurion.com>
2014-10-27 04:06:29 +0100

    Removed old headers form SKProtobufScanner

--- Ice/SteamKit/Packet/SKProtobufScanner.h
+++ Ice/SteamKit/Packet/SKProtobufScanner.h
@@ -7,16 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SKProtobufConstants.h"
 
-typedef NS_ENUM(UInt8, WireType)
-{
-	WireTypeVarint		= 0,
-	WireTypeFixed		= 1,
-	WireTypePacked		= 2,
-	WireTypeDeprecated1 = 3,
-	WireTypeDeprecated2 = 4,
-	WireTypeFloat		= 5
-};
 
 extern NSUInteger const ProtoMask; // AND for isProto, XOR for STRIP
 
